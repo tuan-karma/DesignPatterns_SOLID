16058e65aede0df206a66bf6f540e8283bbd9970
tuan-karma <anhtuanb1@gmail.com>
2023-09-01 18:10:50 +0700

    House keeping

--- Cpp/Hexagon_Guess_My_Number/domain/Display.h
+++
@@ -1,9 +0,0 @@
-// Display.h
-#pragma once
-#include <string>
-
-class Display {
-public:
-    virtual void showMessage(const std::string& message) = 0;
-    virtual ~Display() {}
-};
--- Cpp/Hexagon_Guess_My_Number/domain/Guesses.h
+++
@@ -1,9 +0,0 @@
-// Guesses.h
-#pragma once
-#include <string>
-
-class Guesses {
-public:
-    virtual int getGuess() = 0;
-    virtual ~Guesses() {}
-};
